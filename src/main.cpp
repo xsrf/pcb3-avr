b0d4ddf90ac2f1cc1864bd44d3fbe8a20dad8b13
Andreas Bernhofer <AndreasBernhofer@gmx.de>
2019-12-10 17:25:55 +0100

    two modes working

--- src/main.cpp
+++ src/main.cpp
@@ -1,170 +1,193 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <random.h>
 
-#define LEDS 12
+#define LEDS 12 // LED count
+#define CPLX_MASK 0b00011101 // LED IO mask on PORTB
 
-volatile uint8_t LED[LEDS];
-volatile long animationCount = 0;
-volatile long lastAnimationCount = 0;
-const uint8_t isrPWRvals[3] = { _BV(1), _BV(3), _BV(4) };
+uint8_t LED[LEDS]; // array containing each LEDs brightness (0-7)
+unsigned long globalCounter = 0;
+volatile bool isrTick = true;
+const uint8_t isrPWRvals[3] = { _BV(1), _BV(3), _BV(4) }; // PWM Power/Duration values (LSB to MSB)
+volatile bool btnPressed = false;
 
-void led_on (int var);
-void leds_off ();
+const uint8_t CPLX_PBH[LEDS] = {
+  0b00000001,
+  0b00000100,
+  0b00000100,
+  0b00001000,
+  0b00001000,
+  0b00010000,
+  0b00000001,
+  0b00001000,
+  0b00000100,
+  0b00010000,
+  0b00000001,
+  0b00010000,
+};
+
+uint8_t mode,c_led = 0;
+
+void cplx_led_on(uint8_t var);
+void cplx_off();
+void clear_all();
+void ani_single_order(uint8_t submode);
+void ani_single_flash_order(uint8_t submode);
+void ani_pulse(uint8_t submode);
+void tick();
+
+// See https://blog.podkalicki.com/attiny13-pseudo-random-numbers/
+uint16_t random_number = 0x123;
+uint16_t lfsr16_next(uint16_t n) {
+    return (n >> 0x01U) ^ (-(n & 0x01U) & 0xB400U);
+}
+uint16_t random(void) {
+    return (random_number = lfsr16_next(random_number)); 
+}
 
 int main() {
-  PORTB |= _BV(PB1); // input
-  DDRB &= ~_BV(PB1);
+  // Setup Mode-Button on PB1 (Push-Button to GND)
+  DDRB &= ~_BV(PB1); // Input
+  PORTB |= _BV(PB1); // Enable Pull-Up 
+  // Setup Timer0 / Main PWM and Counter
+  TCCR0A |= _BV(WGM01); // Timer0 to CTC mode (count to OCR0A)
+  TCCR0B |= _BV(CS02); // Prescaler /256
+  TIMSK0 |= _BV(OCIE0A); // enable Timer0 CTC interrupt
+  TIFR0 |= _BV(OCF0A); // enable Interrupt on TOP
+  sei(); // enable interrupts
 
-  TCCR0A |= _BV(WGM01); // set timer counter mode to CTC
-  //TCCR0B |= _BV(CS01)|_BV(CS00); // set prescaler /64
-  TCCR0B |= _BV(CS02); // set prescaler /256
-  OCR0A = 100; // set Timer's counter top value
-  TIMSK0 |= _BV(OCIE0A); // enable Timer CTC interrupt
-  TIFR0 |= _BV(OCF0A); // enable Interrupt on top
-  sei(); // enable global interrupts
-
-  LED[0] = 7;
-  uint8_t ledc = 0;
+  LED[1] = 7;
 
   while(1) {
-    if(lastAnimationCount + 500 < animationCount || lastAnimationCount > animationCount) {
-      lastAnimationCount = animationCount;
-      LED[ledc] = 0;
-      LED[(ledc+7)%LEDS] = 0;
-      ledc = ( ledc + 1 ) % LEDS;
-      LED[ledc] = 7;
-      LED[(ledc+7)%LEDS] = 7;
+    if(isrTick) {
+      isrTick = false;
+      globalCounter++;
+      tick();
     }
   }
+}
 
-
-  while(1) {
-    if(lastAnimationCount + 50 < animationCount || lastAnimationCount > animationCount) {
-      lastAnimationCount = animationCount;
-      for(uint8_t led=4; led<5; led++) {
-        if(LED[led] <= 7) LED[led]++;
-        if(LED[led] > 8) LED[led]--;
-        if(LED[led] == 8) LED[led] = 0xF7;
-        if(LED[led] == 0xF0) LED[led] = 0;
-      }
+void tick() {
+  static uint16_t btnPressCnt = 0;
+  if(btnPressed) { 
+    // Button pressed
+    btnPressCnt++;
+    if((btnPressCnt & 0x1F) < 7) {
+      LED[1] = 7;
+    } else {
+      LED[1] = 1;
     }
-  }
-  for(uint8_t led=0; led<LEDS; led++) LED[led] = led;
-  while(1) {
-    if(lastAnimationCount + 50 < animationCount || lastAnimationCount > animationCount) {
-      lastAnimationCount = animationCount;
-      for(uint8_t led=0; led<LEDS; led++) {
-        if(LED[led] <= 7) LED[led]++;
-        if(LED[led] > 8) LED[led]--;
-        if(LED[led] == 8) LED[led] = 0xF7;
-        if(LED[led] == 0xF0) LED[led] = 0;
-      }
+  } else {
+    if(btnPressCnt > 0) {
+      btnPressCnt = 0;
+      mode++;
+      LED[1] = 0;
+      globalCounter = 0;
+    }
+    // modes
+    switch (mode) {
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+        ani_single_order(mode);
+        break;     
+      case 4:   
+      case 5:
+      case 6:
+      case 7:
+        ani_single_flash_order(mode-4);
+        break;
+      case 8:
+        ani_pulse(mode-8);
+        break;
+      default:
+        mode = 0;
+        break;
     }
   }
 }
 
 
-
-
-
-void leds_off() {
-  PORTB &= ~(
-            _BV(PB0) |
-            _BV(PB2) |
-            _BV(PB3) |
-            _BV(PB4)
-          );
-  DDRB &= ~(
-            _BV(PB0) |
-            _BV(PB2) |
-            _BV(PB3) |
-            _BV(PB4)
-          );
+void ani_single_order(uint8_t submode) {
+  static uint8_t ledIdx = 0;
+  if(globalCounter > 10 + 10*submode) {
+    globalCounter = 0;
+    LED[ledIdx] = 0;
+    ledIdx += 1;
+    if(ledIdx == LEDS) ledIdx = 0;
+    LED[ledIdx] = 7;
+  }
 }
 
-void led_on (int var) {
-  switch (var) {
-    case 0: // 1
-      DDRB |= _BV(PB0) | _BV(PB2); // output
-      PORTB |= _BV(PB0); // HIGH
-      PORTB &= ~_BV(PB2); // LOW
-      break;
-    case 1: // 2
-      DDRB |= _BV(PB0) | _BV(PB2); // output
-      PORTB |= _BV(PB2); // HIGH
-      PORTB &= ~_BV(PB0); // LOW
-      break;
-    case 2: // 3
-      DDRB |= _BV(PB2) | _BV(PB3); // output
-      PORTB |= _BV(PB2); // HIGH
-      PORTB &= ~_BV(PB3); // LOW
-      break;
-    case 3: // 4
-      DDRB |= _BV(PB2) | _BV(PB3); // output
-      PORTB |= _BV(PB3); // HIGH
-      PORTB &= ~_BV(PB2); // LOW
-      break;
-    case 4: // 5
-      DDRB |= _BV(PB3) | _BV(PB4); // output
-      PORTB |= _BV(PB3); // HIGH
-      PORTB &= ~_BV(PB4); // LOW
-      break;
-    case 5: // 6
-      DDRB |= _BV(PB3) | _BV(PB4); // output
-      PORTB |= _BV(PB4); // HIGH
-      PORTB &= ~_BV(PB3); // LOW
-      break;
-    case 6: // 7
-      DDRB |= _BV(PB0) | _BV(PB3); // output
-      PORTB |= _BV(PB0); // HIGH
-      PORTB &= ~_BV(PB3); // LOW
-      break;
-    case 7: // 8
-      DDRB |= _BV(PB0) | _BV(PB3); // output
-      PORTB |= _BV(PB3); // HIGH
-      PORTB &= ~_BV(PB0); // LOW
-      break;
-    case 8: // 9
-      DDRB |= _BV(PB2) | _BV(PB4); // output
-      PORTB |= _BV(PB2); // HIGH
-      PORTB &= ~_BV(PB4); // LOW
-      break;
-    case 9: // 10
-      DDRB |= _BV(PB2) | _BV(PB4); // output
-      PORTB |= _BV(PB4); // HIGH
-      PORTB &= ~_BV(PB2); // LOW
-      break;
-    case 10: // 11
-      DDRB |= _BV(PB0) | _BV(PB4); // output
-      PORTB |= _BV(PB0); // HIGH
-      PORTB &= ~_BV(PB4); // LOW
-      break;
-    case 11: // 12
-      DDRB |= _BV(PB0) | _BV(PB4); // output
-      PORTB |= _BV(PB4); // HIGH
-      PORTB &= ~_BV(PB0); // LOW
-      break;
-    default:
-      ;
+void ani_single_flash_order(uint8_t submode) {
+  static uint8_t ledIdx = 0;
+  if(globalCounter == 4) {
+    LED[ledIdx] = 0;
   }
+  if(globalCounter > 5 + 5*submode) {
+    globalCounter = 0;
+    ledIdx += 1;
+    if(ledIdx == LEDS) ledIdx = 0;
+    LED[ledIdx] = 7;
+  }
+}
+
+void ani_pulse(uint8_t submode) {
+  uint8_t val;
+  if(globalCounter==0) {
+    for(uint8_t i=0; i<LEDS; i++) LED[i] = (random() & 0x07) | 0x10;
+  }
+  for(uint8_t i=0; i<LEDS; i++) {
+    val = LED[i];
+    if(val <= 0x10 && ((random() & 0x1F) == 0)) val++;
+    if(val <= 0x18 && val>0x10) val++;
+    if(val > 0x18) val--;
+    if(val == 0x18) val = 0x27;
+    if(val == 0x20) val = 0x10;
+    LED[i] = val;
+  }
+}
+
+
+
+void clear_all() {
+  for(uint8_t i = 0; i < LEDS; i++) LED[i] = 0;
+}
+
+
+void cplx_off() {
+  PORTB &= ~CPLX_MASK; // GND all (clears charge on pin)
+  DDRB &= ~CPLX_MASK; // input all
+}
+
+void cplx_led_on (uint8_t var) {
+  DDRB |= CPLX_PBH[var | 1];
+  DDRB |= CPLX_PBH[var & ~1];
+  PORTB |= CPLX_PBH[var];
 }
 
 ISR(TIM0_COMPA_vect) {
   static uint8_t isrLEDidx = 0; // the LED lit during this cycle
   static uint8_t isrPWRidx = 0; // the duration/power index the LED is lit ()
+  static uint8_t isrCnt = 0; // Count interrupts before isrTick
   uint8_t skipCnt = 0;
 
   OCR0A = isrPWRvals[isrPWRidx]; // set how long LED will stay on
-  leds_off();
-  if( LED[isrLEDidx] & ( 1 << isrPWRidx ) ) led_on(isrLEDidx);
+  cplx_off();
+  if( LED[isrLEDidx] & ( 1 << isrPWRidx ) ) cplx_led_on(isrLEDidx);
+
+  // Inc global counter every LEDS*3 cycles which should always be same duration
+  if( isrCnt++ == LEDS*6 - 1 ) {
+    btnPressed = !(PINB & _BV(PINB1));
+    isrCnt = 0;
+    isrTick = true;
+  }
 
   // Calc idx and timer duration for next interrupt
-  isrLEDidx = ( isrLEDidx + 1 ) % LEDS;
-  if(isrLEDidx == 0) isrPWRidx = ( isrPWRidx + 1 ) % 3;
-  if(isrPWRidx == 0 && isrPWRidx == 0) animationCount++;
-  // Skip to next lit LED
-  while( LED[isrLEDidx] == 0 && skipCnt++ < LEDS) {
-    isrLEDidx = ( isrLEDidx + 1 ) % LEDS;
-    if(isrLEDidx == 0) isrPWRidx = ( isrPWRidx + 1 ) % 3;
-  }
+  do {
+    isrLEDidx += 1;
+    if(isrLEDidx == LEDS) isrLEDidx = 0;
+    if(isrLEDidx == 0) isrPWRidx = ( isrPWRidx + 1 ) & 3;
+  } while ( LED[isrLEDidx] == 0 && skipCnt++ < LEDS );
 }
